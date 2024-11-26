b80aa60f6b1824a26b68c738df7ab0c7c198ffeb
volkov <volkov1305701@yandex.ru>
2024-11-27 00:28:28 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from './components/layout/layout'
+import MainPage from './pages/mainPage/mainPage'
+
+function App() {
+	return (
+		<Layout>
+				<MainPage />
+		</Layout>
+	)
+}
+
+export default App
